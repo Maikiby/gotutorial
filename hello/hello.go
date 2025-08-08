5300d933cca45472e1b89ebed10316c50152e899
Maikiby <maikiby@pm.me>
2025-08-08 00:49:35 -0600

    error handling

--- hello/hello.go
+++ hello/hello.go
@@ -3,12 +3,21 @@ package main
 import (
 
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 
 )
 
 func main() {
-	message := greetings.Hello("Niko") 
+	log.SetPrefix("greetins: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("") 
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
